8fe57d4b1b767b873658524bdf8a4569beb0e621
DAP <dlrayjr89@gmail.com>
2020-06-18 19:28:05 +0000

    bubble sorting is true

--- src/sorting/bubbleSorting.py
+++ src/sorting/bubbleSorting.py
@@ -2,5 +2,17 @@
 Construct the bubble sort algorithm!
 '''
 
+
 def bubble_sort(arr):
-    pass
+    #Traverse through each element
+    for i in range(len(arr)):
+        #loop to compare each element
+        for j in range(len(arr) - i - 1):
+            #If right is greate than the left element
+            if arr[j] > arr[j + 1]:
+                #swapped element if condition is true
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+data = [67, 79, 2, 5, 8, 10]
+bubble_sort(data)
+print(data)
